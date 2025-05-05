89b66e1042b0ce31aacc4cab1a861b467eb411ae
gabrielnogueiraz <gabriel.nogueira00810@gmail.com>
2025-05-05 19:37:18 -0300

    Adição de icones nos cards de habilidades

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import { SkillsSection, SectionTitle, SkillsContent, SkillCard, SkillIcon } from './styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+  faReact, 
+  faNodeJs 
+} from '@fortawesome/free-brands-svg-icons';
+import { 
+  faDatabase, 
+  faTools 
+} from '@fortawesome/free-solid-svg-icons';
 
 const Skills: React.FC = () => {
   return (
@@ -9,28 +18,28 @@ const Skills: React.FC = () => {
         <SkillsContent>
           <SkillCard>
             <SkillIcon>
-              <i className="fab fa-react"></i>
+              <FontAwesomeIcon icon={faReact} />
             </SkillIcon>
             <h3>Frontend</h3>
             <p>React, TypeScript, Next.js, HTML5, CSS3, JavaScript</p>
           </SkillCard>
           <SkillCard>
             <SkillIcon>
-              <i className="fab fa-node-js"></i>
+              <FontAwesomeIcon icon={faNodeJs} />
             </SkillIcon>
             <h3>Backend</h3>
             <p>Node.js, Java, Ruby, Express, RESTful APIs</p>
           </SkillCard>
           <SkillCard>
             <SkillIcon>
-              <i className="fas fa-database"></i>
+              <FontAwesomeIcon icon={faDatabase} />
             </SkillIcon>
             <h3>Bancos de Dados</h3>
-            <p>PostgreSQL, Firebird, SQLite</p>
+            <p>PostgreSQL, Firebase, SQLite, MongoDB</p>
           </SkillCard>
           <SkillCard>
             <SkillIcon>
-              <i className="fas fa-tools"></i>
+              <FontAwesomeIcon icon={faTools} />
             </SkillIcon>
             <h3>DevOps & Ferramentas</h3>
             <p>Docker, Git, Metodologias Ágeis, Office 365</p>
