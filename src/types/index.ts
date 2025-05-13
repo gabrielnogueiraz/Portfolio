2262469e2c5c8c7723ae11ea28d36eb235499552
gabrielnogueiraz <gabriel.nogueira00810@gmail.com>
2025-05-12 21:50:31 -0300

    Atualiza interfaces de tipos para melhor organização e legibilidade

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,37 +1,38 @@
 export interface Skill {
-    icon: string;
-    title: string;
-    items: string[];
-  }
-  
-  export interface Experience {
-    period: string;
-    title: string;
-    company: string;
-    description: string;
-    position: 'left' | 'right';
-  }
-  
-  export interface Project {
-    id: number;
-    title: string;
-    description: string;
-    image: string;
-    tags: string[];
-    demoLink?: string;
-    githubLink?: string;
-  }
-  
-  export interface ContactInfo {
-    icon: string;
-    title: string;
-    content: string;
-    link?: string;
-  }
-  
-  export interface FormData {
-    name: string;
-    email: string;
-    subject: string;
-    message: string;
-  }
+  icon: string;
+  title: string;
+  items: string[];
+}
+
+export interface Experience {
+  period: string;
+  title: string;
+  company: string;
+  description: string;
+  position: "left" | "right";
+}
+
+export interface Project {
+  id: number;
+  title: string;
+  description: string;
+  images: string[];
+  tags: string[];
+  demoLink?: string;
+  githubLink?: string;
+  isCorporate?: boolean;
+}
+
+export interface ContactInfo {
+  icon: string;
+  title: string;
+  content: string;
+  link?: string;
+}
+
+export interface FormData {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+}
